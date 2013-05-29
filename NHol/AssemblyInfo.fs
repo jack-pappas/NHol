5fc30ffd23dd6fe6b5171e5b6017e5d5e61be34b
Jack Pappas <jackp@server.fake>
2013-05-29 10:46:38 -0400

    Added AssemblyInfo.fs for the NHol project.

---
+++ NHol/AssemblyInfo.fs
@@ -0,0 +1,48 @@
+﻿(*
+
+Copyright 2013 Jack Pappas
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+namespace NHol
+
+/// Assembly-level attributes specific to this assembly.
+module private AssemblyInfo =
+    open System.Reflection
+    open System.Resources
+    open System.Runtime.CompilerServices
+    open System.Runtime.InteropServices
+    open System.Security
+    open System.Security.Permissions
+
+    [<assembly: AssemblyTitle("NHol")>]
+    [<assembly: AssemblyDescription("An implementation of higher-order logic (HOL) in F#.")>]
+    [<assembly: NeutralResourcesLanguage("en-US")>]
+    [<assembly: Guid("eb2e2160-60cd-410d-b7bf-a1cb4bd8cc4c")>]
+
+    (*  Makes internal modules, types, and functions visible
+        to the test project so they can be unit-tested. *)
+    #if DEBUG
+    [<assembly: InternalsVisibleTo("NHol.Tests")>]
+    #endif
+
+    (* Dependency hints for Ngen *)
+    [<assembly: DependencyAttribute("FSharp.Core", LoadHint.Always)>]
+    [<assembly: DependencyAttribute("System", LoadHint.Always)>]
+    [<assembly: DependencyAttribute("ExtCore", LoadHint.Always)>]
+
+    // Appease the F# compiler
+    do ()
+
