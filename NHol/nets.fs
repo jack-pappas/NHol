87a087b71ff9ddf359ca73c55b59def4e3b420d6
Phan Anh Dung <phananhdung309@yahoo.com>
2013-06-07 13:55:39 +0200

    Convert nets module to light syntax using Fantomas

--- NHol/nets.fs
+++ NHol/nets.fs
@@ -35,104 +35,148 @@ open NHol.basics
 (* the head is a variable then then whole term is treated as a variable.     *)
 (* ------------------------------------------------------------------------- *)
 
-type term_label = Vnet                          (* variable (instantiable)   *)
-                 | Lcnet of (string * int)      (* local constant            *)
-                 | Cnet of (string * int)       (* constant                  *)
-                 | Lnet of int;;                (* lambda term (abstraction) *)
+type term_label = 
+    | Vnet                      (* variable (instantiable)   *)
+    | Lcnet of (string * int)   (* local constant            *)
+    | Cnet of (string * int)    (* constant                  *)
+    | Lnet of int               (* lambda term (abstraction) *)
 
-type 'a net = Netnode of (term_label * 'a net) list * 'a list;;
+type net<'a> = 
+    | Netnode of (term_label * 'a net) list * 'a list
 
 (* ------------------------------------------------------------------------- *)
 (* The empty net.                                                            *)
 (* ------------------------------------------------------------------------- *)
 
-let empty_net = Netnode([],[]);;
+let empty_net = Netnode([], [])
 
 (* ------------------------------------------------------------------------- *)
 (* Insert a new element into a net.                                          *)
 (* ------------------------------------------------------------------------- *)
 
-let enter =
-  let label_to_store lconsts tm =
-    let op,args = strip_comb tm in
-    if is_const op then Cnet(fst(dest_const op),length args),args
-    else if is_abs op then
-      let bv,bod = dest_abs op in
-      let bod' = if mem bv lconsts then vsubst [genvar(type_of bv),bv] bod
-                 else bod in
-      Lnet(length args),bod'::args
-    else if mem op lconsts then Lcnet(fst(dest_var op),length args),args
-    else Vnet,[] in
-  let rec canon_eq x y =
-    try compare x y = 0 with Failure _ -> false
-  and canon_lt x y =
-    try compare x y < 0 with Failure _ -> false in
-  let rec sinsert x l =
-    if l = [] then [x] else
-    let h = hd l in
-    if canon_eq h x then failwith "sinsert" else
-    if canon_lt x h then x::l else
-    h::(sinsert x (tl l)) in
-  let set_insert x l = try sinsert x l with Failure "sinsert" -> l in
-  let rec net_update lconsts (elem,tms,Netnode(edges,tips)) =
-    match tms with
-      [] -> Netnode(edges,set_insert elem tips)
-    | (tm::rtms) ->
-          let label,ntms = label_to_store lconsts tm in
-          let child,others =
-            try (snd .>>. I) (remove (fun (x,y) -> x = label) edges)
-            with Failure _ -> (empty_net,edges) in
-          let new_child = net_update lconsts (elem,ntms@rtms,child) in
-          Netnode ((label,new_child)::others,tips) in
-  fun lconsts (tm,elem) net -> net_update lconsts (elem,[tm],net);;
+let enter = 
+    let label_to_store lconsts tm = 
+        let op, args = strip_comb tm
+        if is_const op
+        then Cnet(fst(dest_const op), length args), args
+        elif is_abs op
+        then 
+            let bv, bod = dest_abs op
+            let bod' = 
+                if mem bv lconsts
+                then vsubst [genvar(type_of bv), bv] bod
+                else bod
+            Lnet(length args), bod' :: args
+        elif mem op lconsts
+        then Lcnet(fst(dest_var op), length args), args
+        else Vnet, []
+    let rec canon_eq x y = 
+        try 
+            compare x y = 0
+        with
+        | Failure _ -> false
+    and canon_lt x y = 
+        try 
+            compare x y < 0
+        with
+        | Failure _ -> false
+    let rec sinsert x l = 
+        if l = []
+        then [x]
+        else 
+            let h = hd l
+            if canon_eq h x
+            then failwith "sinsert"
+            elif canon_lt x h
+            then x :: l
+            else h :: (sinsert x (tl l))
+    let set_insert x l = 
+        try 
+            sinsert x l
+        with
+        | Failure "sinsert" -> l
+    let rec net_update lconsts (elem, tms, Netnode(edges, tips)) = 
+        match tms with
+        | [] -> Netnode(edges, set_insert elem tips)
+        | (tm :: rtms) -> 
+            let label, ntms = label_to_store lconsts tm
+            let child, others = 
+                try 
+                    (snd .>>. I)(remove (fun (x, y) -> x = label) edges)
+                with
+                | Failure _ -> (empty_net, edges)
+            let new_child = net_update lconsts (elem, ntms @ rtms, child)
+            Netnode((label, new_child) :: others, tips)
+    fun lconsts (tm, elem) net -> net_update lconsts (elem, [tm], net)
 
 (* ------------------------------------------------------------------------- *)
 (* Look up a term in a net and return possible matches.                      *)
 (* ------------------------------------------------------------------------- *)
 
-let lookup =
-  let label_for_lookup tm =
-    let op,args = strip_comb tm in
-    if is_const op then Cnet(fst(dest_const op),length args),args
-    else if is_abs op then Lnet(length args),(body op)::args
-    else Lcnet(fst(dest_var op),length args),args in
-  let rec follow (tms,Netnode(edges,tips)) =
-    match tms with
-      [] -> tips
-    | (tm::rtms) ->
-          let label,ntms = label_for_lookup tm in
-          let collection =
-            try let child = assoc label edges in
-                follow(ntms @ rtms, child)
-            with Failure _ -> [] in
-          if label = Vnet then collection else
-          try collection @ follow(rtms,assoc Vnet edges)
-          with Failure _ -> collection in
-  fun tm net -> follow([tm],net);;
+let lookup = 
+    let label_for_lookup tm = 
+        let op, args = strip_comb tm
+        if is_const op
+        then Cnet(fst(dest_const op), length args), args
+        elif is_abs op
+        then Lnet(length args), (body op) :: args
+        else Lcnet(fst(dest_var op), length args), args
+    let rec follow(tms, Netnode(edges, tips)) = 
+        match tms with
+        | [] -> tips
+        | (tm :: rtms) -> 
+            let label, ntms = label_for_lookup tm
+            let collection = 
+                try 
+                    let child = assoc label edges
+                    follow(ntms @ rtms, child)
+                with
+                | Failure _ -> []
+            if label = Vnet
+            then collection
+            else 
+                try 
+                    collection @ follow(rtms, assoc Vnet edges)
+                with
+                | Failure _ -> collection
+    fun tm net -> follow([tm], net)
 
 (* ------------------------------------------------------------------------- *)
 (* Function to merge two nets (code from Don Syme's hol-lite).               *)
 (* ------------------------------------------------------------------------- *)
 
-let merge_nets =
-  let rec canon_eq x y =
-    try compare x y = 0 with Failure _ -> false
-  and canon_lt x y =
-    try compare x y < 0 with Failure _ -> false in
-  let rec set_merge l1 l2 =
-    if l1 = [] then l2
-    else if l2 = [] then l1 else
-    let h1 = hd l1 in let t1 = tl l1 in
-    let h2 = hd l2 in let t2 = tl l2 in
-    if canon_eq h1 h2 then h1::(set_merge t1 t2)
-    else if canon_lt h1 h2 then h1::(set_merge t1 l2)
-    else h2::(set_merge l1 t2) in
-  let rec merge_nets (Netnode(l1,data1),Netnode(l2,data2)) =
-    let add_node ((lab,net) as p) l =
-      try let (lab',net'),rest = remove (fun (x,y) -> x = lab) l in
-          (lab',merge_nets (net,net'))::rest
-      with Failure _ -> p::l in
-    Netnode(itlist add_node l2 (itlist add_node l1 []),
-            set_merge data1 data2) in
-  merge_nets;;
-
+let merge_nets = 
+    let rec canon_eq x y = 
+        try 
+            compare x y = 0
+        with
+        | Failure _ -> false
+    and canon_lt x y = 
+        try 
+            compare x y < 0
+        with
+        | Failure _ -> false
+    let rec set_merge l1 l2 = 
+        if l1 = []
+        then l2
+        elif l2 = []
+        then l1
+        else 
+            let h1 = hd l1
+            let t1 = tl l1
+            let h2 = hd l2
+            let t2 = tl l2
+            if canon_eq h1 h2
+            then h1 :: (set_merge t1 t2)
+            elif canon_lt h1 h2
+            then h1 :: (set_merge t1 l2)
+            else h2 :: (set_merge l1 t2)
+    let rec merge_nets(Netnode(l1, data1), Netnode(l2, data2)) = 
+        let add_node ((lab, net) as p) l = 
+            try 
+                let (lab', net'), rest = remove (fun (x, y) -> x = lab) l
+                (lab', merge_nets(net, net')) :: rest
+            with
+            | Failure _ -> p :: l
+        Netnode(itlist add_node l2 (itlist add_node l1 []), set_merge data1 data2)
+    merge_nets
