928a3562725b753f5eb7366fde430a867087a04e
Jack Pappas <jackp@server.fake>
2013-06-23 08:57:05 -0400

    Replaced uses of (o), (&), and the double-semicolon. Reformatted some parts of the code to use a more F#-friendly style. The code is still commented out because it doesn't compile -- there are some uses of 'in' remaining which seem to dramatically affect the way the F# compiler parses this code.

--- NHol/equal.fs
+++ NHol/equal.fs
@@ -21,23 +21,31 @@ limitations under the License.
 /// Basic equality reasoning including conversionals.
 module NHol.equal
 
-(*
+open FSharp.Compatibility.OCaml
+open lib
+open fusion
+open basics
+open nets
+open printer
+open preterm
+open parser
 
+(*
 (* ------------------------------------------------------------------------- *)
 (* Type abbreviation for conversions.                                        *)
 (* ------------------------------------------------------------------------- *)
 
-type conv = term->thm;;
+type conv = term->thm
 
 (* ------------------------------------------------------------------------- *)
 (* A bit more syntax.                                                        *)
 (* ------------------------------------------------------------------------- *)
 
-let lhand = rand o rator;;
+let lhand = rand << rator
 
-let lhs = fst o dest_eq;;
+let lhs = fst << dest_eq
 
-let rhs = snd o dest_eq;;
+let rhs = snd << dest_eq
 
 (* ------------------------------------------------------------------------- *)
 (* Similar to variant, but even avoids constants, and ignores types.         *)
@@ -45,13 +53,13 @@ let rhs = snd o dest_eq;;
 
 let mk_primed_var =
   let rec svariant avoid s =
-    if mem s avoid or (can get_const_type s & not(is_hidden s)) then
+    if mem s avoid || (can get_const_type s && not(is_hidden s)) then
       svariant avoid (s^"'")
-    else s in
+    else s
   fun avoid v ->
     let s,ty = dest_var v in
-    let s' = svariant (mapfilter (fst o dest_var) avoid) s in
-    mk_var(s',ty);;
+    let s' = svariant (mapfilter (fst << dest_var) avoid) s
+    mk_var(s',ty)
 
 (* ------------------------------------------------------------------------- *)
 (* General case of beta-conversion.                                          *)
@@ -59,10 +67,10 @@ let mk_primed_var =
 
 let BETA_CONV tm =
   try BETA tm with Failure _ ->
-  try let f,arg = dest_comb tm in
-      let v = bndvar f in
+  try let f,arg = dest_comb tm
+      let v = bndvar f
       INST [arg,v] (BETA (mk_comb(f,v)))
-  with Failure _ -> failwith "BETA_CONV: Not a beta-redex";;
+  with Failure _ -> failwith "BETA_CONV: Not a beta-redex"
 
 (* ------------------------------------------------------------------------- *)
 (* A few very basic derived equality rules.                                  *)
@@ -70,41 +78,41 @@ let BETA_CONV tm =
 
 let AP_TERM tm th =
   try MK_COMB(REFL tm,th)
-  with Failure _ -> failwith "AP_TERM";;
+  with Failure _ -> failwith "AP_TERM"
 
 let AP_THM th tm =
   try MK_COMB(th,REFL tm)
-  with Failure _ -> failwith "AP_THM";;
+  with Failure _ -> failwith "AP_THM"
 
 let SYM th =
-  let tm = concl th in
-  let l,r = dest_eq tm in
-  let lth = REFL l in
-  EQ_MP (MK_COMB(AP_TERM (rator (rator tm)) th,lth)) lth;;
+  let tm = concl th
+  let l,r = dest_eq tm
+  let lth = REFL l
+  EQ_MP (MK_COMB(AP_TERM (rator (rator tm)) th,lth)) lth
 
 let ALPHA tm1 tm2 =
   try TRANS (REFL tm1) (REFL tm2)
-  with Failure _ -> failwith "ALPHA";;
+  with Failure _ -> failwith "ALPHA"
 
 let ALPHA_CONV v tm =
-  let res = alpha v tm in
-  ALPHA tm res;;
+  let res = alpha v tm
+  ALPHA tm res
 
 let GEN_ALPHA_CONV v tm =
   if is_abs tm then ALPHA_CONV v tm else
-  let b,abs = dest_comb tm in
-  AP_TERM b (ALPHA_CONV v abs);;
+  let b,abs = dest_comb tm
+  AP_TERM b (ALPHA_CONV v abs)
 
 let MK_BINOP op (lth,rth) =
-  MK_COMB(AP_TERM op lth,rth);;
+  MK_COMB(AP_TERM op lth,rth)
 
 (* ------------------------------------------------------------------------- *)
 (* Terminal conversion combinators.                                          *)
 (* ------------------------------------------------------------------------- *)
 
-let (NO_CONV:conv) = fun tm -> failwith "NO_CONV";;
+let (NO_CONV:conv) = fun tm -> failwith "NO_CONV"
 
-let (ALL_CONV:conv) = REFL;;
+let (ALL_CONV:conv) = REFL
 
 (* ------------------------------------------------------------------------- *)
 (* Combinators for sequencing, trying, repeating etc. conversions.           *)
@@ -112,79 +120,87 @@ let (ALL_CONV:conv) = REFL;;
 
 let ((THENC):conv -> conv -> conv) =
   fun conv1 conv2 t ->
-    let th1 = conv1 t in
-    let th2 = conv2 (rand(concl th1)) in
-    TRANS th1 th2;;
+    let th1 = conv1 t
+    let th2 = conv2 (rand(concl th1))
+    TRANS th1 th2
 
 let ((ORELSEC):conv -> conv -> conv) =
-  fun conv1 conv2 t ->
-    try conv1 t with Failure _ -> conv2 t;;
+    fun conv1 conv2 t ->
+        try conv1 t
+        with Failure _ ->
+            conv2 t
 
-let (FIRST_CONV:conv list -> conv) = end_itlist (fun c1 c2 -> c1 ORELSEC c2);;
+let (FIRST_CONV:conv list -> conv) = end_itlist (fun c1 c2 -> c1 ORELSEC c2)
 
 let (EVERY_CONV:conv list -> conv) =
-  fun l -> itlist (fun c1 c2 -> c1 THENC c2) l ALL_CONV;;
+  fun l -> itlist (fun c1 c2 -> c1 THENC c2) l ALL_CONV
 
 let REPEATC =
-  let rec REPEATC conv t =
-    ((conv THENC (REPEATC conv)) ORELSEC ALL_CONV) t in
-  (REPEATC:conv->conv);;
+    let rec REPEATC conv t =
+        ((conv THENC (REPEATC conv)) ORELSEC ALL_CONV) t
+    (REPEATC:conv->conv)
 
 let (CHANGED_CONV:conv->conv) =
   fun conv tm ->
-    let th = conv tm in
-    let l,r = dest_eq (concl th) in
-    if aconv l r then failwith "CHANGED_CONV" else th;;
+    let th = conv tm
+    let l,r = dest_eq (concl th)
+    if aconv l r then failwith "CHANGED_CONV" else th
 
-let TRY_CONV conv = conv ORELSEC ALL_CONV;;
+let TRY_CONV conv = conv ORELSEC ALL_CONV
 
 (* ------------------------------------------------------------------------- *)
 (* Subterm conversions.                                                      *)
 (* ------------------------------------------------------------------------- *)
 
 let (RATOR_CONV:conv->conv) =
-  fun conv tm ->
-    let l,r = dest_comb tm in AP_THM (conv l) r;;
+    fun conv tm ->
+        let l,r = dest_comb tm
+        AP_THM (conv l) r
 
 let (RAND_CONV:conv->conv) =
-  fun conv tm ->
-    let l,r = dest_comb tm in AP_TERM l (conv r);;
+    fun conv tm ->
+        let l,r = dest_comb tm
+        AP_TERM l (conv r)
 
-let LAND_CONV = RATOR_CONV o RAND_CONV;;
+let LAND_CONV = RATOR_CONV << RAND_CONV
 
 let (COMB2_CONV: conv->conv->conv) =
-  fun lconv rconv tm -> let l,r = dest_comb tm in MK_COMB(lconv l,rconv r);;
+    fun lconv rconv tm ->
+        let l,r = dest_comb tm
+        MK_COMB(lconv l,rconv r)
 
-let COMB_CONV = W COMB2_CONV;;
+let COMB_CONV = W COMB2_CONV
 
 let (ABS_CONV:conv->conv) =
-  fun conv tm ->
-    let v,bod = dest_abs tm in
-    let th = conv bod in
-    try ABS v th with Failure _ ->
-    let gv = genvar(type_of v) in
-    let gbod = vsubst[gv,v] bod in
-    let gth = ABS gv (conv gbod) in
-    let gtm = concl gth in
-    let l,r = dest_eq gtm in
-    let v' = variant (frees gtm) v in
-    let l' = alpha v' l and r' = alpha v' r in
-    EQ_MP (ALPHA gtm (mk_eq(l',r'))) gth;;
+    fun conv tm ->
+        let v,bod = dest_abs tm
+        let th = conv bod
+        try ABS v th
+        with Failure _ ->
+            let gv = genvar(type_of v)
+            let gbod = vsubst[gv,v] bod
+            let gth = ABS gv (conv gbod)
+            let gtm = concl gth
+            let l,r = dest_eq gtm
+            let v' = variant (frees gtm) v
+            let l' = alpha v' l
+            let r' = alpha v' r
+            EQ_MP (ALPHA gtm (mk_eq(l',r'))) gth
 
 let BINDER_CONV conv tm =
-  if is_abs tm then ABS_CONV conv tm
-  else RAND_CONV(ABS_CONV conv) tm;;
+    if is_abs tm then ABS_CONV conv tm
+    else RAND_CONV(ABS_CONV conv) tm
 
 let SUB_CONV conv tm =
-  match tm with
-    Comb(_,_) -> COMB_CONV conv tm
-  | Abs(_,_) -> ABS_CONV conv tm
-  | _ -> REFL tm;;
+    match tm with
+    | Comb(_,_) -> COMB_CONV conv tm
+    | Abs(_,_) -> ABS_CONV conv tm
+    | _ -> REFL tm
 
 let BINOP_CONV conv tm =
-  let lop,r = dest_comb tm in
-  let op,l = dest_comb lop in
-  MK_COMB(AP_TERM op (conv l),conv r);;
+    let lop,r = dest_comb tm
+    let op,l = dest_comb lop
+    MK_COMB(AP_TERM op (conv l),conv r)
 
 (* ------------------------------------------------------------------------- *)
 (* Depth conversions; internal use of a failure-propagating `Boultonized'    *)
@@ -196,7 +212,7 @@ let (ONCE_DEPTH_CONV: conv->conv),
     (REDEPTH_CONV: conv->conv),
     (TOP_DEPTH_CONV: conv->conv),
     (TOP_SWEEP_CONV: conv->conv) =
-  let THENQC conv1 conv2 tm =
+  let rec THENQC conv1 conv2 tm =
     try let th1 = conv1 tm in
         try let th2 = conv2(rand(concl th1)) in TRANS th1 th2
         with Failure _ -> th1
@@ -234,7 +250,7 @@ let (ONCE_DEPTH_CONV: conv->conv),
   (fun c -> TRY_CONV (DEPTH_QCONV c)),
   (fun c -> TRY_CONV (REDEPTH_QCONV c)),
   (fun c -> TRY_CONV (TOP_DEPTH_QCONV c)),
-  (fun c -> TRY_CONV (TOP_SWEEP_QCONV c));;
+  (fun c -> TRY_CONV (TOP_SWEEP_QCONV c))
 
 (* ------------------------------------------------------------------------- *)
 (* Apply at leaves of op-tree; NB any failures at leaves cause failure.      *)
@@ -242,12 +258,12 @@ let (ONCE_DEPTH_CONV: conv->conv),
 
 let rec DEPTH_BINOP_CONV op conv tm =
   match tm with
-    Comb(Comb(op',l),r) when op' = op ->
-      let l,r = dest_binop op tm in
-      let lth = DEPTH_BINOP_CONV op conv l
-      and rth = DEPTH_BINOP_CONV op conv r in
+  | Comb(Comb(op',l),r) when op' = op ->
+      let l,r = dest_binop op tm
+      let rec lth = DEPTH_BINOP_CONV op conv l
+      and rth = DEPTH_BINOP_CONV op conv r
       MK_COMB(AP_TERM op' lth,rth)
-  | _ -> conv tm;;
+  | _ -> conv tm
 
 (* ------------------------------------------------------------------------- *)
 (* Follow a path.                                                            *)
@@ -259,8 +275,8 @@ let PATH_CONV =
       [] -> cnv
     | "l"::t -> RATOR_CONV (path_conv t cnv)
     | "r"::t -> RAND_CONV (path_conv t cnv)
-    | _::t -> ABS_CONV (path_conv t cnv) in
-  fun s cnv -> path_conv (explode s) cnv;;
+    | _::t -> ABS_CONV (path_conv t cnv)
+  fun s cnv -> path_conv (explode s) cnv
 
 (* ------------------------------------------------------------------------- *)
 (* Follow a pattern                                                          *)
@@ -273,67 +289,74 @@ let PAT_CONV =
     else if is_comb pat then
       COMB2_CONV (PCONV xs (rator pat) conv) (PCONV xs (rand pat) conv)
     else
-      ABS_CONV (PCONV xs (body pat) conv) in
-  fun pat -> let xs,pbod = strip_abs pat in PCONV xs pbod;;
+      ABS_CONV (PCONV xs (body pat) conv)
+  fun pat ->
+    let xs,pbod = strip_abs pat
+    PCONV xs pbod
 
 (* ------------------------------------------------------------------------- *)
 (* Symmetry conversion.                                                      *)
 (* ------------------------------------------------------------------------- *)
 
 let SYM_CONV tm =
-  try let th1 = SYM(ASSUME tm) in
-      let tm' = concl th1 in
-      let th2 = SYM(ASSUME tm') in
+  try let th1 = SYM(ASSUME tm)
+      let tm' = concl th1
+      let th2 = SYM(ASSUME tm')
       DEDUCT_ANTISYM_RULE th2 th1
-  with Failure _ -> failwith "SYM_CONV";;
+  with Failure _ -> failwith "SYM_CONV"
 
 (* ------------------------------------------------------------------------- *)
 (* Conversion to a rule.                                                     *)
 (* ------------------------------------------------------------------------- *)
 
 let CONV_RULE (conv:conv) th =
-  EQ_MP (conv(concl th)) th;;
+  EQ_MP (conv(concl th)) th
 
 (* ------------------------------------------------------------------------- *)
 (* Substitution conversion.                                                  *)
 (* ------------------------------------------------------------------------- *)
 
 let SUBS_CONV ths tm =
-  try if ths = [] then REFL tm else
-      let lefts = map (lhand o concl) ths in
-      let gvs = map (genvar o type_of) lefts in
-      let pat = subst (zip gvs lefts) tm in
-      let abs = list_mk_abs(gvs,pat) in
-      let th = rev_itlist
-        (fun y x -> CONV_RULE (RAND_CONV BETA_CONV THENC LAND_CONV BETA_CONV)
-                              (MK_COMB(x,y))) ths (REFL abs) in
-      if rand(concl th) = tm then REFL tm else th
-  with Failure _ -> failwith "SUBS_CONV";;
+    try
+        if ths = [] then REFL tm
+        else
+            let lefts = map (lhand << concl) ths
+            let gvs = map (genvar << type_of) lefts
+            let pat = subst (zip gvs lefts) tm
+            let abs = list_mk_abs(gvs,pat)
+            let th = rev_itlist
+                      (fun y x -> CONV_RULE (RAND_CONV BETA_CONV THENC LAND_CONV BETA_CONV)
+                                    (MK_COMB(x,y))) ths (REFL abs)
+            if rand(concl th) = tm then REFL tm else th
+    with Failure _ ->
+        failwith "SUBS_CONV"
 
 (* ------------------------------------------------------------------------- *)
 (* Get a few rules.                                                          *)
 (* ------------------------------------------------------------------------- *)
 
-let BETA_RULE = CONV_RULE(REDEPTH_CONV BETA_CONV);;
+let BETA_RULE = CONV_RULE(REDEPTH_CONV BETA_CONV)
 
-let GSYM = CONV_RULE(ONCE_DEPTH_CONV SYM_CONV);;
+let GSYM = CONV_RULE(ONCE_DEPTH_CONV SYM_CONV)
 
-let SUBS ths = CONV_RULE (SUBS_CONV ths);;
+let SUBS ths = CONV_RULE (SUBS_CONV ths)
 
 (* ------------------------------------------------------------------------- *)
 (* A cacher for conversions.                                                 *)
 (* ------------------------------------------------------------------------- *)
 
 let CACHE_CONV =
-  let ALPHA_HACK th =
-    let tm' = lhand(concl th) in
-    fun tm -> if tm' = tm then th else TRANS (ALPHA tm tm') th in
-  fun conv ->
-    let net = ref empty_net in
-    fun tm -> try tryfind (fun f -> f tm) (lookup tm (!net))
-              with Failure _ ->
-                  let th = conv tm in
-                  (net := enter [] (tm,ALPHA_HACK th) (!net); th);;
-
+    let ALPHA_HACK th =
+        let tm' = lhand(concl th)
+        fun tm -> if tm' = tm then th else TRANS (ALPHA tm tm') th
+    fun conv ->
+        let net = ref empty_net
+        fun tm ->
+            try tryfind (fun f -> f tm) (lookup tm (!net))
+            with Failure _ ->
+                let th = conv tm
+                net := enter [] (tm,ALPHA_HACK th) (!net)
+                th
 *)
+
 
