0745881255c1e3b7191ebc442d9cde8d646d8407
Jack Pappas <jackp@server.fake>
2013-07-07 10:53:10 -0400

    Imported strongly-typed fluent assertion functions from ExtCore.Tests.

--- NHol.Tests/TestHelpers.fs
+++ NHol.Tests/TestHelpers.fs
@@ -23,6 +23,64 @@ open System
 open NUnit.Framework
 open FsCheck
 
+(* Fluent test helpers for use with NUnit and FsUnit. *)
+
+/// Tests that the specified condition is true.
+/// If not, calls Assert.Fail with a formatted string.
+let inline assertf (condition : bool) format : 'T =
+    Printf.ksprintf (fun str -> if not condition then Assert.Fail str) format
+
+/// Asserts that two values are equal.
+let inline assertEqual<'T when 'T : equality> (expected : 'T) (actual : 'T) =
+    Assert.AreEqual (expected, actual)
+
+/// Asserts that two values are NOT equal.
+let inline assertNotEqual<'T when 'T : equality> (expected : 'T) (actual : 'T) =
+    Assert.AreNotEqual (expected, actual)
+
+/// Asserts that two objects are identical.
+let inline assertSame<'T when 'T : not struct> (expected : 'T) (actual : 'T) =
+    Assert.AreSame (expected, actual)
+
+/// Asserts that two objects are NOT identical.
+let inline assertNotSame<'T when 'T : not struct> (expected : 'T) (actual : 'T) =
+    Assert.AreNotSame (expected, actual)
+
+/// Asserts that a condition is true.
+let inline assertTrue condition =
+    Assert.IsTrue (condition)
+
+/// Asserts that a condition is false.
+let inline assertFalse condition =
+    Assert.IsFalse (condition)
+
+
+/// Assertion functions for collections.
+[<RequireQualifiedAccess>]
+module Collection =
+    open System.Collections
+
+    /// Asserts that two collections are exactly equal.
+    /// The collections must have the same count, and contain the exact same objects in the same order.
+    let inline assertEqual<'T, 'U when 'T :> seq<'U>> (expected : 'T) (actual : 'T) =
+        CollectionAssert.AreEqual (expected, actual)
+
+    /// Asserts that two collections are not exactly equal.
+    let inline assertNotEqual<'T, 'U when 'T :> seq<'U>> (expected : 'T) (actual : 'T) =
+        CollectionAssert.AreNotEqual (expected, actual)
+
+    /// Asserts that two collections are exactly equal.
+    /// The collections must have the same count, and contain the exact same objects but the match may be in any order.
+    let inline assertEquiv<'T, 'U when 'T :> seq<'U>> (expected : 'T) (actual : 'T) =
+        CollectionAssert.AreEquivalent (expected, actual)
+
+    /// Asserts that two collections are not exactly equal.
+    let inline assertNotEquiv<'T, 'U when 'T :> seq<'U>> (expected : 'T) (actual : 'T) =
+        CollectionAssert.AreNotEquivalent (expected, actual)
+
+
+(* Fluent test helpers for use with NUnit and FsCheck. *)
+
 /// An FsCheck runner which reports FsCheck test results to NUnit.
 type private NUnitRunner () =
     interface IRunner with
