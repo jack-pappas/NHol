0eb3e92108ab5ce2419b952f5c7c8b0e32b816bc
domasin <domenico.masini@gmail.com>
2013-07-14 18:07:27 +0200

    insert theorems without proving them with BUGGY condition till real module

--- Tutorial/exploration.fsx
+++ Tutorial/exploration.fsx
@@ -1,4 +1,22 @@
-﻿#I "./../packages"
+﻿(*
+
+Copyright 2013 Anh-Dung Phan, Domenico Masini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+#I "./../packages"
 
 #r "FSharp.Compatibility.OCaml.0.1.10/lib/net40/FSharp.Compatibility.OCaml.dll"
 #r "FSharp.Compatibility.OCaml.Format.0.1.10/lib/net40/FSharp.Compatibility.OCaml.Format.dll"
@@ -41,18 +59,18 @@ open NHol.pair //pair module has to be checked
 open NHol.nums
 open NHol.recursion
 open NHol.arith   
-open NHol.wf
+open NHol.wf //
 open NHol.calc_num
 open NHol.normalizer
 open NHol.grobner
-//open NHol.ind_types stackOverFlow
-//open NHol.lists    needs ind_types
+open NHol.ind_types //
+open NHol.lists
 open NHol.realax   
 open NHol.calc_int 
 open NHol.realarith
-open NHol.real    
-open NHol.calc_rat 
-open NHol.int     
+open NHol.real  //  
+//open NHol.calc_rat 
+//open NHol.int     
 //open NHol.sets     
 //open NHol.iterate
 //open NHol.cart     
@@ -83,14 +101,18 @@ ARITH_ZERO;;                // forces calc_num module evaluation
 SEMIRING_NORMALIZERS_CONV;; // forces normalizer module evaluation
 RING_AND_IDEAL_CONV;;       // forces grobner module evaluation
 
-//INJ_INVERSE2;;      //ind_types
-//LIST_INDUCT_TAC;;   // lists
+INJ_INVERSE2;;      //ind_types
+LIST_INDUCT_TAC;;   // lists
 dist;;              //relax
 mk_realintconst;;   //calc_int
 REAL_LTE_TOTAL;;    //realarith
-REAL_OF_NUM_LT;;    //real
-DECIMAL;;           //calc_rat
-integer;;           //int
+REAL_OF_NUM_LT;;    //real various
+
+#load "calc_rat.fs" 
+open NHol.calc_rat 
+
+//DECIMAL;;           //calc_rat first issue at line 120
+//integer;;           //int
 //IN;;                //sets
 //FINITE_NUMSEG;;     //iterate
 //dimindex;;          //cart
