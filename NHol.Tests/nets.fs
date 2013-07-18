334cd4f243bbad5d22da9fb38dc43c07baaaf3af
domasin <domenico.masini@gmail.com>
2013-07-18 08:58:24 +0200

    unit test for enter function

--- NHol.Tests/nets.fs
+++ NHol.Tests/nets.fs
@@ -19,12 +19,63 @@ limitations under the License.
 /// Tests for functions in the NHol.nets module.
 module Tests.NHol.nets
 
+open NHol.fusion
+open NHol.nets
+
 open NUnit.Framework
 open FsUnit
 
 [<Test>]
 let dummy () : unit =
     Assert.Ignore "Test not yet implemented."
+
+[<Test>]
+let ``{enter} insert a new element into a net``() =
+
+    let aTerm = Const ("a",Tyvar "bool")
+    let bTerm = Const ("b",Tyvar "bool")
+
+    //a = b
+    let equalityTerm = 
+        Comb
+            (Comb
+               (Const
+                  ("=",
+                   Tyapp
+                     ("fun",[Tyvar "A"; Tyapp ("fun",[Tyvar "A"; Tyapp ("bool",[])])])), aTerm),bTerm)
+
+    let MY_CONV (x:term) = Sequent([], equalityTerm)
+
+    let expected = Netnode([(Cnet("a", 0), Netnode([], [MY_CONV]))], [])
+
+    let actual = enter [] (aTerm,MY_CONV) empty_net
+
+    actual
+    |> should equal expected
+
+//[<Test>]
+//let ``{lookup} looks up a term in a net and return possible matches``() =
+//
+//    let aTerm = Const ("a",Tyvar "bool")
+//    let bTerm = Const ("b",Tyvar "bool")
+//
+//    //a = b
+//    let equalityTerm = 
+//        Comb
+//            (Comb
+//               (Const
+//                  ("=",
+//                   Tyapp
+//                     ("fun",[Tyvar "A"; Tyapp ("fun",[Tyvar "A"; Tyapp ("bool",[])])])), aTerm),bTerm)
+//
+//    let MY_CONV (x:term) = Sequent([], equalityTerm)
+//
+//    let termNet = Netnode([(Cnet("a", 0), Netnode([], [MY_CONV]))], [])
+//
+//    let actual = lookup bTerm termNet
+//
+//    actual
+//    |> should equal bTerm
 
 
 
