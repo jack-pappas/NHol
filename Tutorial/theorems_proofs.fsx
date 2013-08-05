5aa711ec37f9f57a0319c1597f424bd1458c0ed5
domasin <domenico.masini@gmail.com>
2013-08-05 16:47:52 +0200

    Analysing EQ_TRANS proof

--- Tutorial/theorems_proofs.fsx
+++ Tutorial/theorems_proofs.fsx
@@ -62,40 +62,95 @@ mk_rewrites;;               // forces simp module evaluation
 
 (* EQ_REFL: |- !x:A. x = x *)
 
-// Explicit proof
+// Forward proof
 let EQ_REFL0 = REFL (parse_term @"x");;                 // |- x = x
 let EQ_REFL = GEN (parse_term @"x") EQ_REFL0;;          // |- !x. x = x
 
-// Tactics based proof
+// Backward proof
 g (parse_term @"!x:A. x = x");;                         // 1 subgoal: !x. x = x
 e GEN_TAC;;                                             // 1 subgoal: x = x
 e REFL_TAC;;                                            // No subgoals
 
 (* REFL_CLAUSE: |- !x:A. (x = x) <=> T *)
 
-// Explicit proof
+// Forward proof
 let REFL_CLAUSE0 = EQ_REFL                              // |- !x. x = x
 let REFL_CLAUSE1 = SPEC_ALL REFL_CLAUSE0                // |- x = x
 let REFL_CLAUSE2 = EQT_INTRO REFL_CLAUSE1               // |- x = x <=> T
 let REFL_CLAUSE = GEN (parse_term @"x") REFL_CLAUSE2    // |- !x. x = x <=> T
 
-// Tactics based proof
+// Backward proof
 g (parse_term @"!x:A. (x = x) <=> T");;                 // 1 subgoal: !x. x = x <=> T)
 e GEN_TAC;;                                             // 1 subgola: x = x <=> T)
 e (MATCH_ACCEPT_TAC(EQT_INTRO(SPEC_ALL EQ_REFL)));;     // No subgoals
 
 (* EQ_SYM: |- !(x:A) y. (x = y) ==> (y = x) *)
 
-// Explicit proof
+// Forward proof
 let EQ_SYM0 = ASSUME (parse_term @"(x:A = y)")          // x = y |- x = y
 let EQ_SYM1 = SYM EQ_SYM0                               // x = y |- y = x
 let EQ_SYM2 = DISCH (parse_term @"(x:A = y)") EQ_SYM1   // |- x = y ==> y = x Note that declaring x to be of type A is necessary for this to work
 let EQ_SYM3 = GEN (parse_term @"y") EQ_SYM2             // |- !y. x = y ==> y = x
 let EQ_SYM = GEN (parse_term @"x") EQ_SYM3              // |- !x y. x = y ==> y = x
 
-// Tactics based proof
+// Backward proof
 g (parse_term @"!(x:A) y. (x = y) ==> (y = x)");;       // 1 subgoal: !x y. x = y ==> y = x
 e (REPEAT GEN_TAC);;                                    // 1 subgoal: x = y ==> y = x
 e (DISCH_THEN(ACCEPT_TAC << SYM));;                     // No Subgoal
 
-(* EQ_SYM_EQ: |- !(x:A) y. (x = y) ==> (y = x) *)
+(* EQ_SYM_EQ: |- !(x:A) y. (x = y) <=> (y = x) *)
+
+// Forward proof
+let EQ_SYM_EQ0 = SPEC_ALL EQ_SYM                                                                // |- x = y ==> y = x
+let EQ_SYM_EQ1 = 
+    INST 
+        [(parse_term @"x:A"),(parse_term @"y:A");(parse_term @"y:A"),(parse_term @"x:A")] 
+        EQ_SYM_EQ0                                                                              // |- y = x ==> x = y
+let EQ_SYM_EQ2 = IMP_ANTISYM_RULE EQ_SYM_EQ0 EQ_SYM_EQ1                                         // |- x = y <=> y = x
+let EQ_SYM_EQ3 = GEN (parse_term @"y") EQ_SYM_EQ2                                               // |- !y. x = y <=> y = x
+let EQ_SYM_EQ4 = GEN (parse_term @"x") EQ_SYM_EQ3                                               // |- !x y. x = y <=> y = x
+
+// Backward proof
+g (parse_term @"!(x:A) y. (x = y) <=> (y = x)");;                                               // 1 subgoal:  !x y. x = y <=> y = x)
+e (REPEAT GEN_TAC);;                                                                            // 1 subgoal:  x = y <=> y = x
+e EQ_TAC;;                                                                                      // 2 subgoals:
+                                                                                                //             x = y ==> y = x
+                                                                                                //             y = x ==> x = y
+e (MATCH_ACCEPT_TAC EQ_SYM);;                                                                   // 1 subgoal:  y = x ==> x = y 
+e (MATCH_ACCEPT_TAC EQ_SYM);;                                                                   // No subgoal         
+
+(* EQ_TRANS: |- !(x:A) y z. (x = y) /\ (y = z) ==> (x = z) *)
+
+// Forward proof
+let EQ_TRANS0 = ASSUME (parse_term @"(x:A = y) /\ (y = z)");;                                   // x = y /\ y = z |- x = y /\ y = z
+let EQ_TRANS1,EQ_TRANS2 = CONJ_PAIR EQ_TRANS0;;                                                 // x = y /\ y = z |- y = z, x = y /\ y = z |- x = y
+let EQ_TRANS3 = TRANS EQ_TRANS1 EQ_TRANS2;;                                                     // x = y /\ y = z |- x = z
+let EQ_TRANS4 = DISCH (parse_term @"(x:A = y) /\ (y = z)") EQ_TRANS3;;                          // |- x = y /\ y = z ==> x = z
+let EQ_TRANS5 = GEN (parse_term @"y") EQ_TRANS4                                                 // |- !y. x = y /\ y = z ==> x = z
+let EQ_TRANS = GEN (parse_term @"x") EQ_TRANS5                                                  // |- !x y. x = y /\ y = z ==> x = z
+
+// Backward proof
+g (parse_term @"!(x:A) y z. (x = y) /\ (y = z) ==> (x = z)");;
+e STRIP_TAC;; // removes x with GEN_TAC
+e STRIP_TAC;; // removes y with GEN_TAC
+e STRIP_TAC;; // removes z with GEN_TAC
+//e STRIP_TAC;; // this fails: STRIP_TAC tries to apply GEN_TAC, CONJ_TAC and (DISCH_THEN STRIP_ASSUME_TAC) in order: if one fails then tries the next, but at the first fails here it doesn't try the next
+e (DISCH_TAC);; // this is what e STRIP_TAC was supposed to do explicitly to remove the outermost implication and put the antecedent as an assumption. Probabily together with CONJ_TAC simultanously which anyway is not needed
+e (PURE_ASM_REWRITE_TAC []);;
+e REFL_TAC;;
+
+// Backward proof more explicit
+g (parse_term @"!(x:A) y z. (x = y) /\ (y = z) ==> (x = z)");;
+e GEN_TAC;;
+e GEN_TAC;;
+e GEN_TAC;;
+e (DISCH_TAC);;
+e (PURE_ASM_REWRITE_TAC []);;
+e REFL_TAC;;
+
+let EQ_TRANS_revisited = 
+    prove
+        ((parse_term @"!(x:A) y z. (x = y) /\ (y = z) ==> (x = z)"), 
+         REPEAT GEN_TAC |> THEN <| DISCH_TAC
+         |> THEN <| PURE_ASM_REWRITE_TAC []
+         |> THEN <| REFL_TAC)
