132aeef06e144fe00fbf17b554f7f40746991e7e
Eric Taucher <Researcher0x00@gmail.com>
2013-08-18 10:43:52 -0400

    Add check for NHol.dll and friendly message if not found.
    Signed-off-by: Eric Taucher <Researcher0x00@gmail.com>

--- NHol.bat
+++ NHol.bat
@@ -3,6 +3,10 @@
 
 @echo off
 
+:: Get location of required dll: NHol.dll
+set NHOL_RELATIVE=NHol\bin\Debug\NHol.dll
+set "NHOL_ABSOLUTE=%~dp0%NHOL_RELATIVE%"
+
 :: Variables which hold paths to various versions of F# Interactive.
 set FSI_30= "C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\fsi.exe"
 set FSI_20= "C:\Program Files (x86)\Microsoft F#\v4.0\fsi.exe"
@@ -59,31 +63,49 @@ set fsi_args= --lib:NHol^
   --use:help.fs^
   --use:database.fs^
 
-:: Try to find the user's installation of F# Interactive.
-if exist %FSI_30% (
-    :: VS 2012 / F# 3.0
-    echo Using F# 3.0
+:: Verify NHol.dll exists before proceeding
+if exist %NHOL_ABSOLUTE% (
 
-    :: Set the 'FSI' variable to the path for F# 3.0 Interactive.
-    set FSI= %FSI_30%
+   :: Try to find the user's installation of F# Interactive.
+   if exist %FSI_30% (
+      :: VS 2012 / F# 3.0
+      echo Using F# 3.0
 
-    set FSI_VER_FLAG = ""
-) else (
-if exist %FSI_20% (
-    :: VS 2010 / F# 2.0
-    echo Using F# 2.0
+      :: Set the 'FSI' variable to the path for F# 3.0 Interactive.
+      set FSI= %FSI_30%
 
-    :: Set the 'FSI' variable to the path for F# 2.0 Interactive.
-    set FSI= %FSI_20%
+      set FSI_VER_FLAG = ""
+   ) else (
+      if exist %FSI_20% (
+          :: VS 2010 / F# 2.0
+          echo Using F# 2.0
 
+          :: Set the 'FSI' variable to the path for F# 2.0 Interactive.
+          set FSI= %FSI_20%
 	  
-    set FSI_VER_FLAG = "--define:FSI_VER_2"
+          set FSI_VER_FLAG = "--define:FSI_VER_2"
+      ) else (
+          :: Unable to find the F# installation, so exit.
+          echo Unable to find an installation of F# interactive at any known location.
+          echo Exiting...
+          pause
+          exit
+      )
+   )
 ) else (
-    :: Unable to find the F# installation, so exit.
-    echo Unable to find an installation of F# interactive at any known location.
-    echo Exiting...
-    exit
-))
+
+:: Unable to find NHol.dll so exit.
+echo Unable to find NHol.dll at %NHOL_ABSOLUTE%.
+echo.
+echo Did you build NHol.dll by compiling the project?
+echo If not it is suggested to use Visual Studio to automatically download NuGet packages.
+echo Note: NHol works with Visual Studio Express which is free.
+echo.
+echo Exiting...
+echo.
+pause
+exit
+)
 
 :: Load the NHol environment into the detected version of F# Interactive.
 echo Loading NHol into F# Interactive.
