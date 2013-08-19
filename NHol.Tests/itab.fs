44bbfe486d6b49e98b2b5d1b2c9655f55c147d4b
Phan Anh Dung <phananhdung309@yahoo.com>
2013-08-19 09:43:59 +0200

    Add test cases for itab and simp module

---
+++ NHol.Tests/itab.fs
@@ -0,0 +1,55 @@
+﻿(*
+
+Copyright 2013 Anh-Dung Phan, Domenico Masini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+/// Tests for functions in the NHol.itab module.
+module Tests.NHol.itab
+
+open NHol.lib
+open NHol.fusion
+open NHol.parser
+open NHol.printer
+open NHol.equal
+open NHol.bool
+open NHol.tactics
+open NHol.itab
+open NHol.simp
+open NHol.``class``
+open NHol.meson
+
+open NUnit.Framework
+
+[<Test>]
+let ``{ITAUT_TAC} Simple intuitionistic logic prover``() =
+    let _ = g <| parse_term @"!p q. (p ==> q) <=> (~q ==> ~p)"
+    let _ = e (REPEAT GEN_TAC |> THEN <| EQ_TAC)
+    let _ = e ITAUT_TAC
+    let gs = e (MESON_TAC [])
+
+    List.isEmpty gs
+    |> assertEqual true
+
+//// This crashes VS test runner
+//[<Test>]
+//let ``{ITAUT} Attempt to prove term using intuitionistic first-order logic``() =
+//    let actual = ITAUT <| parse_term @"~(~(~p)) ==> ~p"
+//    let expected = Sequent([], parse_term @"~(~(~p)) ==> ~p")
+//
+//    actual
+//    |> evaluate
+//    |> assertEqual expected
+
---
+++ NHol.Tests/simp.fs
@@ -0,0 +1,53 @@
+﻿(*
+
+Copyright 2013 Anh-Dung Phan, Domenico Masini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+/// Tests for functions in the NHol.simp module.
+module Tests.NHol.simp
+
+open NHol.lib
+open NHol.fusion
+open NHol.parser
+open NHol.printer
+open NHol.equal
+open NHol.bool
+open NHol.tactics
+open NHol.itab
+open NHol.simp
+open NHol.theorems
+open NHol.``class``
+open NHol.meson
+
+open NUnit.Framework
+
+[<Test>]
+let ``{REWR_CONV} Uses an instance of a given equation to rewrite a term``() =
+    let actual = REWR_CONV EQ_SYM_EQ <| parse_term @"(T : bool) = F"
+    let expected = Sequent([], parse_term @"(T : bool) = F <=> F = T")
+
+    actual
+    |> evaluate
+    |> assertEqual expected
+
+[<Test>]
+[<ExpectedException(typeof<System.Exception>, ExpectedMessage = "term_pmatch")>]
+let ``{REWR_CONV} Fails on unmatched terms``() =
+    let actual = REWR_CONV EQ_SYM_EQ <| parse_term @"(T : bool) ==> F"
+
+    actual
+    |> evaluate
+    |> ignore
