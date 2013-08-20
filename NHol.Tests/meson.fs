503247437b12813f542ab80f0e5c65dd930fc057
Phan Anh Dung <phananhdung309@yahoo.com>
2013-08-20 09:47:29 +0200

    Add placeholders for some test modules

---
+++ NHol.Tests/meson.fs
@@ -0,0 +1,44 @@
+﻿(*
+
+Copyright 2013 Anh-Dung Phan, Domenico Masini
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*)
+
+/// Tests for functions in the NHol.meson module.
+module Tests.NHol.meson
+
+open NHol.lib
+open NHol.fusion
+open NHol.parser
+open NHol.printer
+open NHol.equal
+open NHol.bool
+open NHol.tactics
+open NHol.itab
+open NHol.simp
+open NHol.theorems
+open NHol.``class``
+
+open NUnit.Framework
+
+//[<Test>]
+//let ``{MESON} doesn't fail on this simple term``() =
+//    let actual = MESON [] <| parse_term @"?!n. n = m"
+//    let expected = Sequent ([], parse_term @"?!n. n = m")
+//
+//    actual
+//    |> evaluate
+//    |> assertEqual expected
+
