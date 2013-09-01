6592fff59b7d7a799595adbd029b2ca690d878c3
Jack Pappas <jackp@server.fake>
2013-09-01 16:56:06 -0400

    Removed the code which checks if NHol.dll has been built, because it was only there to ensure NuGet packages were available before running NHol interactively; now, we just use the new 'restore' command available in NuGet 2.7 to restore packages without building NHol.dll.

--- NHol.bat
+++ NHol.bat
@@ -2,19 +2,48 @@
 :: latest version of F# Interactive (FSI) available on this machine.
 
 @echo off
-setlocal enabledelayedexpansion
 
-:: Get location of required dll: NHol.dll
-set NHOL_RELATIVE=NHol\bin\Debug\NHol.dll
-set "NHOL_ABSOLUTE=%~dp0%NHOL_RELATIVE%"
+:: Restore NuGet packages (if necessary) before proceeding.
+.nuget\NuGet.exe restore NHol\packages.config -PackagesDirectory packages
+
+if %errorlevel% neq 0 (
+    echo Unable to restore NuGet packages.
+    echo Exiting...
+    exit /b %errorlevel%
+)
 
 :: Variables which hold paths to various versions of F# Interactive.
 set FSI_30= "C:\Program Files (x86)\Microsoft SDKs\F#\3.0\Framework\v4.0\fsi.exe"
 set FSI_20= "C:\Program Files (x86)\Microsoft F#\v4.0\fsi.exe"
 
+:: Try to find the user's installation of F# Interactive.
+if exist %FSI_30% (
+  :: VS 2012 / F# 3.0
+  echo Using F# 3.0
+
+  :: Set the 'FSI' variable to the path for F# 3.0 Interactive.
+  set FSI= %FSI_30%
+  set FSI_ARGS= %FSI_ARGS% --define:FSI_VER_3
+
+) else (
+  if exist %FSI_20% (
+      :: VS 2010 / F# 2.0
+      echo Using F# 2.0
+
+      :: Set the 'FSI' variable to the path for F# 2.0 Interactive.
+      set FSI= %FSI_20%
+      set FSI_ARGS= %FSI_ARGS% --define:FSI_VER_2
+
+  ) else (
+      :: Unable to find the F# installation, so exit.
+      echo Unable to find an installation of F# interactive at any known location.
+      echo Exiting...
+      exit
+  )
+)
+
 :: Set variables which hold command-line arguments to be passed to F# Interactive.
 set fsi_args= --lib:NHol^
-  fsi_ver^
   --define:DEBUG^
   --define:TRACE^
   --define:USE^
@@ -62,51 +91,8 @@ set fsi_args= --lib:NHol^
   --use:cart.fs^
   --use:define.fs^
   --use:help.fs^
-  --use:database.fs^
-
-:: Verify NHol.dll exists before proceeding
-if exist %NHOL_ABSOLUTE% (
-
-   :: Try to find the user's installation of F# Interactive.
-   if exist %FSI_30% (
-      :: VS 2012 / F# 3.0
-      echo Using F# 3.0
-
-      :: Set the 'FSI' variable to the path for F# 3.0 Interactive.
-      set FSI= %FSI_30%
-      set fsi_args=!fsi_args:fsi_ver=--define:FSI_VER_3!
-
-   ) else (
-      if exist %FSI_20% (
-          :: VS 2010 / F# 2.0
-          echo Using F# 2.0
-
-          :: Set the 'FSI' variable to the path for F# 2.0 Interactive.
-          set FSI= %FSI_20%
-          set fsi_args=!fsi_args:fsi_ver=--define:FSI_VER_2!
-
-      ) else (
-          :: Unable to find the F# installation, so exit.
-          echo Unable to find an installation of F# interactive at any known location.
-          echo Exiting...
-          pause
-          exit
-      )
-   )
-) else (
-
-:: Unable to find NHol.dll so exit.
-echo Unable to find NHol.dll at %NHOL_ABSOLUTE%.
-echo.
-echo Did you build NHol.dll by compiling the project?
-echo If not it is suggested to use Visual Studio to automatically download NuGet packages.
-echo Note: NHol works with Visual Studio Express which is free.
-echo.
-echo Exiting...
-echo.
-pause
-exit
-)
+  --use:database.fs
+  :: NOTE : The last filename should NOT have a trailing ^ character!
 
 :: Load the NHol environment into the detected version of F# Interactive.
 echo Loading NHol into F# Interactive.
